38f818002f75806451b44b5172ba91418a0ac699
inaw-tihor <rohit.b.wani@gmail.com>
2025-07-28 03:28:48 +0530

    Update backup.py
    parameterising DBInstanceIdentifier variable

--- helper/rds/backup.py
+++ helper/rds/backup.py
@@ -3,8 +3,9 @@ from datetime import datetime
 
 rds = boto3.client('rds')
 snapshot_id = "manual-snap-" + datetime.now().strftime('%Y%m%d%H%M%S')
+db_instance_identifier="rds-postgres-minimal-rdsinstance-mxdcpdxhiu5l"
 
 rds.create_db_snapshot(
-    DBInstanceIdentifier="rds-postgres-minimal-rdsinstance-mxdcpdxhiu5l",
+    DBInstanceIdentifier=db_instance_identifier,
     DBSnapshotIdentifier=snapshot_id
 )
